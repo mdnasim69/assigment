a14415a302b67d6b88cb7d936517b8831477b18b
mdnasim69 <mdnasimnasim2007@gmail.com>
2024-09-27 23:25:22 +0600

    livetest

--- lib/bin/livetest.dart
+++ lib/bin/livetest.dart
@@ -1,3 +1,20 @@
-void main(){
-  print(5994);
+class Car {
+  String brand;
+  String model;
+  int year;
+
+  Car({required this.brand, required this.model, required this.year});
+
+  int carAge() {
+    return 2024 - year;
+  }
+}
+
+void main() {
+  Car myCar = Car(year:  2015,brand: 'Toyota', model: 'Corolla',);
+
+  print('Brand: ${myCar.brand}');
+  print('Model: ${myCar.model}');
+  print('Year: ${myCar.year}');
+  print('Car Age: ${myCar.carAge()} years');
 }
